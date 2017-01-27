cdd4efddd6c17de636b600b02f61cb759a2127de
charmesal <kevindullens3@hotmail.com>
2017-01-27 02:42:51 +0100

    versie 0.1

---
+++ ScrambleRenamerForza/ScrambleRenamerForza/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ScrambleRenamerForza
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFolder.TabIndex = 0;
+            this.btnOpenFolder.Text = "Open folder";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnOpenFolder_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(94, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(689, 238);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 261);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnOpenFolder);
+            this.Name = "Form1";
+            this.Text = "Dennis\' lelijke hoofd";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ ScrambleRenamerForza/ScrambleRenamerForza/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ScrambleRenamerForza
+{
+    public partial class Form1 : Form
+    {
+        List<string> dirs;
+        public Form1()
+        {
+            InitializeComponent();
+            dirs = new List<string>();
+        }
+
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+                    renameFilesRecursive(fbd.SelectedPath);
+                    MessageBox.Show("kappa", "done");
+                }
+            }
+        }
+
+        private void renameFilesRecursive(string selectedPath)
+        {
+            List<string> dirs = new List<string>();
+            List<string> files = new List<string>();
+
+            dirs = Directory.GetDirectories(selectedPath).ToList();
+            files = Directory.GetFiles(selectedPath).ToList();
+
+            foreach (String s in dirs)
+            {
+                string renamedFolder = "";
+                DirectoryInfo di = new DirectoryInfo(s);
+                renameFilesRecursive(s);
+                try
+                {
+                    renamedFolder = '\\' + rename(di.Name);
+                    Directory.Move(di.FullName, selectedPath + renamedFolder);
+
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message + "\nquitting");
+                    Application.Exit();
+                }
+                listBox1.Items.Add(di.Name + "\t->\t" + renamedFolder);
+                listBox1.Items.Add("");
+
+                listBox1.Refresh();
+
+            }
+            listBox1.Items.Add(selectedPath + ":");
+            listBox1.Refresh();
+
+            foreach (String fs in files)
+            {
+                if (File.Exists(fs))
+                {
+                    string renamedFile = "";
+                    FileInfo fi = new FileInfo(fs);
+                    try
+                    {
+                        renamedFile = '\\' + rename(fi.Name);
+                        File.Move(fi.FullName, fi.DirectoryName +renamedFile);
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBox.Show(e.Message + "\nquitting");
+                        Application.Exit();
+                    }
+                    listBox1.Items.Add("\t" + fi.Name + "\t->\t" + renamedFile);
+                    listBox1.Refresh();
+                }
+            }
+        }
+
+        private string rename( string fs)
+        {
+            string newString = "";
+            for(int i = 0; i< fs.Length; i++)
+            {
+                newString += checkTable(fs[i]);
+            }
+            return newString;
+        }
+
+        private char checkTable(char c)
+        {
+            char returnval = '\0';
+            char[] scrabmled =
+            {
+                'l', '`', '^', '6', 'q', 'v', '{', '@', '$', '7', 's', 'b', 'g', '8', 'h', 'u', 'f', '4', '~', '1', '=', '\'', 'm', ']', '!', ',', 'y', '_', '[', '0', 'w', 'k', '(', '2', 'j', '}', ';', '+', '.'
+            };
+
+            char[] unscrambled =
+            {
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'
+            };
+
+            for(int i = 0; i < scrabmled.Length; i++)
+            {
+                if(scrabmled[i] == c)
+                {
+                    returnval = unscrambled[i];
+                    break;
+                }
+            }
+
+            return returnval;
+        }
+    }
+}
